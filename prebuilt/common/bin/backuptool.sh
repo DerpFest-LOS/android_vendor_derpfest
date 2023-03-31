a2495ee074dd9b712a6276fbe7f3b4ca39ba3b1e
storm119 <storm119@gmail.com>
2023-04-01 02:17:11 +0530

    May the GOD bless my ancestors...
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- prebuilt/common/bin/backuptool.sh
+++ prebuilt/common/bin/backuptool.sh
@@ -7,7 +7,6 @@ export C=/tmp/backupdir
 export SYSDEV="$(readlink -nf "$2")"
 export SYSFS="$3"
 export TMPDIR=/tmp
-export V=22
 
 export ADDOND_VERSION=3
 
@@ -47,10 +46,6 @@ check_prereq() {
 if [ ! -r $S/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
-fi
-if ! grep -q "^ro.lineage.version=$V.*" $S/build.prop; then
-  echo "Backup/restore is not possible. Incompatible ROM version: $V"
-  return 2
 fi
 return 0
 }
