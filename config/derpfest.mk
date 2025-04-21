6262159803cd1d2de36aceeb6e602b5362a3c9b3
ppanzenboeck <philipp.panzenboeck1@gmail.com>
2025-04-21 18:54:00 +0200

    config: Update TARGET_NOT_USES_BLUR
    now actually behaves as intended: if set to true, blurs are disabled and
    the toggle is gone from settings; if set to anything other than true, the
    switch is still present in settings; if not set at all, blurs are enabled
    and the switch is present in settings.
    
    Change-Id: I9fbcb849fa160dab8f243f3d28e13e640daeb642

--- config/derpfest.mk
+++ config/derpfest.mk
@@ -16,14 +16,23 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
 # Blur
-ifeq ($(TARGET_NOT_USES_BLUR), false)
-    PRODUCT_SYSTEM_EXT_PROPERTIES += \
-        ro.sf.blurs_are_expensive=1 \
-        ro.surface_flinger.supports_background_blur=1
-else
-    PRODUCT_PRODUCT_PROPERTIES += \
-        ro.launcher.blur.appLaunch=0
+ifndef TARGET_NOT_USES_BLUR
+    USES_BLUR=1
 endif
+
+ifeq ($(TARGET_NOT_USES_BLUR),true)
+    USES_BLUR=0
+else
+    USES_BLUR=1
+endif
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=$(USES_BLUR) \
+    ro.surface_flinger.supports_background_blur=$(USES_BLUR) \
+    persist.sysui.disableBlur=$(shell echo $$((1 - $(USES_BLUR))))
+
+PRODUCT_PRODUCT_PROPERTIES += \
+     ro.launcher.blur.appLaunch=0
 
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
